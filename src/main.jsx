4c9c5e02163db9de6a7241b2475908e618f2e4bd
Pawarasasmina <pawarasasmina2002@icloud.com>
2025-04-21 03:54:06 +0530

    2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import './app.css'
+
 import { UserProvider } from './contexts/UserContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
